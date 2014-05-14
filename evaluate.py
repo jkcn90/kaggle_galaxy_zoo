63e9a0b28332336aca16ea4181cf49a0b9f85bf6
Jeffrey Ning <jkcn90@gmail.com>
2014-05-14 17:53:52 -0400

    b

--- evaluate.py
+++ evaluate.py
@@ -17,28 +17,30 @@ def get_rmse(actual, predicted):
     print('RMSE: ' + str(100*rmse) + '%')
     return rmse
 
-def cross_validate(clf, X, y, cv):
+def cross_validate(clf, X, y, cv=5):
+    if cv < 2:
+        raise Exception("cv must be greater than 2")
     index = X.index
     num_images = index.shape[0]
     slice_size = num_images / cv
 
     rmse_list = []
-
+    
     for i in range(0, cv):
+        # Setup indicies
         validate_index = index[i*slice_size:(i+1)*slice_size]
         train_index = index.drop(validate_index)
 
         X_validate = X.ix[validate_index]
         X_train = X.ix[train_index]
-
+        
         y_validate = y.ix[validate_index]
         y_train = y.ix[train_index]
-
+        
+        # Get rmse
         clf.fit(X_train, y_train)
-
-        (y, y_predicted) = (y_validate.values, clf.predict(X_validate))
-        rmse = math.sqrt(mean_squared_error(y_validate, y_predicted))
+        y_predicted = clf.predict(X_validate)
+        rmse = math.sqrt(metrics.mean_squared_error(y_validate, y_predicted))
         rmse_list.append(rmse)
-
     rmse_list = np.array(rmse_list)
     return rmse_list
